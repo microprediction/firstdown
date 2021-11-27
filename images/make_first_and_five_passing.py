08f2ecceaabdd3b817e7cd69b68397ec45dd1748
Peter Cotton <peter.cotton@microprediction.com>
2021-11-27 10:03:34 -0500

    images

--- images/make_first_and_five_passing.py
+++ images/make_first_and_five_passing.py
@@ -8,7 +8,7 @@ font = {'family' : 'normal',
 import matplotlib
 matplotlib.rc('font', **font)
 
-dpi = 1200
+dpi = 300
 
 gains_on_first = df.loc[(df['dwn']==1)& (df['ytg']==5) & (df['type']=='PASS') & (df['yds']<8) & (df['yds']>1),:]
 gains_on_first[['yds']].rename(columns={'yds':'First Down Yards Gained'}).hist(bins=50)
@@ -16,4 +16,4 @@ import matplotlib.pyplot as plt
 plt.ylabel('Count',fontsize=16)
 plt.xlabel('Yards on first down',fontsize=16)
 plt.title('First and five passing',fontsize=20)
-plt.savefig('first_and_five_passing_'+str(dpi)+'.png', dpi=1200)
+plt.savefig('first_and_five_passing_'+str(dpi)+'.png', dpi=dpi)
